cf64e41524be32a49064abb647a3645e966e7f8f
Md Abdul Ahad <abd.me7@gmail.com>
2026-01-08 19:22:55 +0600

    Find the Maximum Number Function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const prompt = require('prompt-sync')();
+
 // Problem 1: Reverse a String
 
-function reverseString(str){
+function reverseString(){
+    const str = prompt("Enter string for reverse: ");
     let reverse = ''
     for (let i = str.length -1 ; i>=0; i--){
         reverse += str[i];
@@ -8,12 +11,13 @@ function reverseString(str){
     return reverse
 }
 
-// console.log(reverseString('hello'))
+// console.log(reverseString());
 
 
 // Problem 2: Count Vowels in a String
 
-function countVowel (str){
+function countVowel (){
+    const str = prompt("Enter string for Count Vowel: ");
     const vowel = 'aeiouAEIOU'
     let count = 0
     for(let char of str){
@@ -23,16 +27,36 @@ function countVowel (str){
     }
     return count;
 }
-
-// console.log(countVowel("programming"))
+// console.log(countVowel())
 
 
 // Problem 3: Check for Palindrome
 
-function isPalindrome(str){
-    const lowerStr = str.toLowerCase();
+function isPalindrome(){
+    const input = prompt("Enter string check Palindrome: ");
+    const lowerStr = input.toLowerCase();
     return lowerStr === reverseString(lowerStr);
 }
 
-console.log(isPalindrome('madam'))
-console.log(isPalindrome('hello'))
+// console.log(isPalindrome())
+
+
+// Problem 4: Find the Maximum Number
+
+function findMaxNum(){
+    const input = prompt('Enter numbers separated by commas: ');
+    const arr = input.split(',').map(Number);
+
+    if (arr.length === 0) return undefined
+    
+    let max = arr[0];
+
+    for(let i = 1; i<arr.length; i++){
+        if(arr[i]>max){
+            max = arr[i]
+        }
+    }
+    return max;
+}
+
+console.log(findMaxNum()); 
