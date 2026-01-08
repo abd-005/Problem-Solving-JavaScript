7909372b6a2b1f12da203634e0681532c2a06f26
Md Abdul Ahad <abd.me7@gmail.com>
2026-01-09 00:30:11 +0600

    Capitalize First Letter of Each Word

--- index.js
+++ index.js
@@ -1,93 +1,89 @@
-const prompt = require('prompt-sync')();
+const prompt = require("prompt-sync")();
 
 // Problem 1: Reverse a String
 
-function reverseString(){
-    const str = prompt("Enter string for reverse: ");
-    let reverse = ''
-    for (let i = str.length -1 ; i>=0; i--){
-        reverse += str[i];
-    }
-    return reverse
+function reverseString() {
+  const str = prompt("Enter string for reverse: ");
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  return reverse;
 }
 
 // console.log(reverseString());
 
-
 // Problem 2: Count Vowels in a String
 
-function countVowel (){
-    const str = prompt("Enter string for Count Vowel: ");
-    const vowel = 'aeiouAEIOU'
-    let count = 0
-    for(let char of str){
-        if(vowel.includes(char)){
-            count++
-        }
+function countVowel() {
+  const str = prompt("Enter string for Count Vowel: ");
+  const vowel = "aeiouAEIOU";
+  let count = 0;
+  for (let char of str) {
+    if (vowel.includes(char)) {
+      count++;
     }
-    return count;
+  }
+  return count;
 }
 // console.log(countVowel())
 
-
 // Problem 3: Check for Palindrome
 
-function isPalindrome(){
-    const input = prompt("Enter string check Palindrome: ");
-    const lowerStr = input.toLowerCase();
-    return lowerStr === reverseString(lowerStr);
+function isPalindrome() {
+  const input = prompt("Enter string check Palindrome: ");
+  const lowerStr = input.toLowerCase();
+  return lowerStr === reverseString(lowerStr);
 }
 
 // console.log(isPalindrome())
 
-
 // Problem 4: Find the Maximum Number
 
-function findMaxNum(){
-    const input = prompt('Enter numbers separated by commas: ');
-    const arr = input.split(',').map(Number);
+function findMaxNum() {
+  const input = prompt("Enter numbers separated by commas: ");
+  const arr = input.split(",").map(Number);
 
-    if (arr.length === 0) return undefined
-    
-    let max = arr[0];
+  if (arr.length === 0) return undefined;
 
-    for(let i = 1; i<arr.length; i++){
-        if(arr[i]>max){
-            max = arr[i]
-        }
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    return max;
+  }
+  return max;
 }
 
-// console.log(findMaxNum()); 
+// console.log(findMaxNum());
 
 // Problem 5: Remove Duplicates from an Array
 
 function removeDuplicate() {
-    const input = prompt('Enter numbers separated by commas: ');
-    const arr = input.split(',').map(Number);
-    let newArr = [];
-    for (let num of arr) {
-        if (!newArr.includes(num)) {
-            newArr.push(num);
-        }
+  const input = prompt("Enter numbers separated by commas: ");
+  const arr = input.split(",").map(Number);
+  let newArr = [];
+  for (let num of arr) {
+    if (!newArr.includes(num)) {
+      newArr.push(num);
     }
-    return newArr;
+  }
+  return newArr;
 }
 
 // console.log(removeDuplicate());
 
-
 // Problem 6: Sum of All Numbers in an Array
 
 function sum() {
-    const input = prompt('Enter numbers separated by commas: ');
-    const arr = input.split(',').map(Number);
-    let sum = 0;
-    for (let num of arr) {
-        sum += num;
-    }
-    return sum;
+  const input = prompt("Enter numbers separated by commas: ");
+  const arr = input.split(",").map(Number);
+  let sum = 0;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
 }
 
 // console.log(sum());
@@ -95,16 +91,28 @@ function sum() {
 // Problem 7: Find Even Numbers in an Array
 
 function evenNumber() {
-    const input = prompt('Enter numbers separated by commas: ');
-    const arr = input.split(',').map(Number);
+  const input = prompt("Enter numbers separated by commas: ");
+  const arr = input.split(",").map(Number);
 
-    let evenNumber = [];
-    for (let num of arr) {
-        if (num % 2 === 0) {
-            evenNumber.push(num);
-        }
+  let evenNumber = [];
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      evenNumber.push(num);
     }
-    return evenNumber;
+  }
+  return evenNumber;
 }
 
-console.log(evenNumber());
+// console.log(evenNumber());
+
+// Problem 8: Capitalize First Letter of Each Word
+
+function capitalizeFirst() {
+  const str = prompt("Enter string to Capitalize First Letter: ");
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+console.log(capitalizeFirst());
