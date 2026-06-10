4fc3ac6c12bf25a8d201432cf9c633e130601beb
Md Abdul Ahad <abd.me7@gmail.com>
2026-06-10 16:44:12 +0600

    Check Even or Odd — solved in JavaScript

---
+++ Day 3 — Arrays & Array Methods/isEven.js
@@ -0,0 +1,12 @@
+// Problem 2: Check Even or Odd  [Easy]
+// Description: Write a function isEven(n) that returns true if a number is even, and false if it is odd.
+// Example:
+// Input: 4  → Output: trueInput: 7  → Output: false
+// Hint: Use the modulus (%) operator.
+
+
+const isEven = (num) => {
+    return res = num % 2 == 0 ? true : false;
+}
+
+console.log(isEven(7))
